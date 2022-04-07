593c5385e4690adb08ad191007c8cdcedab87204
NAoHR <najmim625@gmail.com>
2022-04-07 16:03:03 +0700

    pre-error

--- containers/Main_container.jsx
+++ containers/Main_container.jsx
@@ -5,6 +5,11 @@ import Stats_card from "../components/main/Stats_card";
 import News_card from "../components/main/News_card";
 
 const NewsCardLooper = ({newsData}) => {
+    if(newsData === false){
+        return <p>wew</p>
+    }else if(newsData.length == 0){
+        return <p>0</p>
+    }
     return newsData.map((val,index)=> {
         return <News_card data={val} key={index} />
     })
