395a59b826d6ec2328649c2943ff03e84010fb93
unknown <yeahbruh199@gmail.com>
2022-04-08 19:21:32 +0700

    damn i just exposed the API key

--- utils/apiCaller.js
+++ utils/apiCaller.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const apiURL = {
-    // nyTimes : `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=financial&api-key=${process.env.NEXT_PUBLIC_NYTIMES}`
-    nyTimes : `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=financial&api-key=8qSZmxm2opJ23cX4I14mc6UpvsbOfuos`
-}
+    nyTimes : `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=financial&api-key=${process.env.NEXT_PUBLIC_NYTIMES}`}
 
 export const apiCaller = {
     nyTimes : () =>  {
