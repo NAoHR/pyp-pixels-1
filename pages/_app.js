e3eedc21662a5a03a2d39426ad4d0d94f131b967
NAoHR <najmim625@gmail.com>
2022-04-11 14:10:29 +0700

    minor change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import TransitionContext from '../utils/Transition_Context';
+import Head from "next/head"
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>pyp-pixels</title>
+      </Head>
       <TransitionContext>
         <Component {...pageProps} />
       </TransitionContext>
